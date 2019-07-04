1681d1b4452c8d321647402b3199910a3067d653
Tricker <tricker.pan@gmail.com>
2019-07-04 17:33:45 +0800

    Init project.

---
+++ code/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::domain('blog.tricker.org')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/hello/{name}', function ($name) {
+        return "Hello, {$name}!";
+    });
+});
+
+Route::domain('admin.tricker.org')->group(function () {
+    Route::get('/', function () {
+        return 'Welcome to Admin dashboard!';
+    });
+});
